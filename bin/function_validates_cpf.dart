a13c5ca9d27a14299d937faa43112091e067a314
charleston <charles1235223@gmail.com>
2023-03-10 08:30:15 -0400

    first commit

---
+++ bin/function_validates_cpf.dart
@@ -0,0 +1,5 @@
+import 'package:function_validates_cpf/function_validates_cpf.dart' as function_validates_cpf;
+
+void main(List<String> arguments) {
+  print('Hello world: ${function_validates_cpf.calculate()}!');
+}
